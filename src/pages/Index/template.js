4b60cbec4f83a7575ebaf6bd15d50559b912a463
ruoyu <hunger@jirengu.com>
2018-05-23 17:24:30 +0800

    blog 接口封装

--- src/pages/Index/template.js
+++ src/pages/Index/template.js
@@ -1,8 +1,10 @@
 import request from '@/helpers/request.js'
 import auth from '@/api/auth.js'
+import blog from '@/api/blog.js'
 
 window.request = request
 window.auth = auth
+window.blog = blog
 
 
 export default {
