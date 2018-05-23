bc5981dab54bb43aff1027a19456a0a7ba35977d
ruoyu <hunger@jirengu.com>
2018-05-23 15:46:58 +0800

    elementui 的使用

--- src/pages/Index/template.js
+++ src/pages/Index/template.js
@@ -3,5 +3,20 @@ export default {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
+  },
+
+  methods: {
+    onClick1() {
+      this.$message.error('错了哦，这是一条错误消息')
+    },
+
+    onClick2() {
+      this.$alert('这是一段内容', '标题名称', {
+          confirmButtonText: '确定',
+          callback: action => {
+            this.$message.success('点了确定')
+          }
+        })
+    }
   }
 }
